4216c3574f32b2676003ddfd6ccd1c52176aecd9
petrussola <pere.solaclaver@gmail.com>
2019-09-03 13:06:17 +0100

    Add keydown event that triggers bingo game: if you type bingo while on the window title will change text and color

--- js/index.js
+++ js/index.js
@@ -8,3 +8,20 @@ document.querySelector(".intro img").addEventListener('mouseover', e => {
 document.querySelector(".intro img").addEventListener('mouseout', e => {
     e.target.style.opacity = 1;
 });
+
+// try typing b i n g o on the window, see what happens :)
+
+const title = document.querySelector(".logo-heading");
+const bingo = [];
+window.addEventListener('keydown', e => {
+    bingo.push(e.keyCode);
+    prize();
+});
+
+function prize() {
+    if (bingo.join() === "66,73,78,71,79") {
+        title.textContent = "Bingo, you've won a prize!";
+        title.style.color = "red";
+    }
+}
+

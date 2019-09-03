26802128f76dc5c1e65469d4fbf0fd4a81f20347
petrussola <pere.solaclaver@gmail.com>
2019-09-03 16:09:14 +0100

    Make width of the box increase when mouse is down

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,13 +1,32 @@
 const blocks = document.querySelectorAll('.blocks div');
 let order = 0;
+let widthBox = 100;
+let count = 0;
 blocks.forEach(item => {
     item.addEventListener('click', e => {
         decreaseOrder();
         e.target.style.order = order;
-    })
+    });
+    item.addEventListener('mousedown', e => {
+        startCounter(e.target);
+    });
+    item.addEventListener('mouseup', e => {
+        stop();
+    });
+    
 })
-console.log(order);
 
 function decreaseOrder() {
-    return order = order - 1;
+    return order -= 1;
+}
+
+function startCounter(param) {
+    function cb() {
+        widthBox += 1;
+        param.style.width = widthBox + "px";
+    };
+    var interval = window.setInterval(cb, 10);
+}
+function stop() {
+    window.clearInterval(interval);
 }

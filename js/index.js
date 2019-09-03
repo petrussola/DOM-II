dd42a1f76d3725e0bd594da478c20e32511654dd
petrussola <pere.solaclaver@gmail.com>
2019-09-03 12:50:03 +0100

    Create mouseover event on img in intro section whereas img goes opacity 0.5. Create mouseout event whereas same img goes back to opacity 1 when mouse is out

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+// img in header goes opacity 0.5 when mouseover event and goes back to opacity 1 when mouseout
+
+document.querySelector(".intro img").addEventListener('mouseover', e => {
+    e.target.style.opacity = 0.5;
+});
+document.querySelector(".intro img").addEventListener('mouseout', e => {
+    e.target.style.opacity = 1;
+});
